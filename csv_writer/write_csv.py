5cbbdb84faf168845ad24ee5f0cb585a4f1b7707
Sam McAlilly <sammcalilly@gmail.com>
2020-06-28 19:01:40 -0500

    changed the temp csv to use memory instead of writing a temp file to disk

--- csv_writer/write_csv.py
+++ csv_writer/write_csv.py
@@ -3,17 +3,17 @@ import os
 from csv import reader
 import pandas as pd
 import time
+from io import StringIO
           
 
 def main():
   arg_map = parse_arguments()
 
-  # build the csv lines and store it in a temp file 
-  # (need to change that to memory instead of disk?)
-  build_csv(arg_map)
+  # build the csv lines and store it in a string
+  tmp_csv = build_tmp_csv(arg_map)
 
   # final csv output
-  sort_and_output_final_csv(arg_map)
+  sort_and_output_final_csv(tmp_csv, arg_map)
 
 
 def parse_arguments():
@@ -40,7 +40,6 @@ def parse_arguments():
   arg_map.update({
     'jurisdiction_list': get_jurisdiction_list(arg_map.get('jurisdiction_list_path')),
     'district': district,
-    'temp_file_name': f'tmp/tmp-{time.strftime("%Y%m%d-%H%M%S")}.csv',
     'results_file_name': f'results-{time.strftime("%Y%m%d-%H%M%S")}.csv',
     'csv_header': [arg_map.get('jurisdiction_level').lower(), 'office', 'district', 'party', 'candidate', 'votes']
   })
@@ -62,24 +61,28 @@ def get_jurisdiction_list(path):
   return jurisdictions
 
 
-def build_csv(arg_map):
+def build_tmp_csv(arg_map):
   """
-    Builds csv lines per candidate.
+    Builds a chunk of csv lines for each candidate.
     
     It opens the candidate_votes.csv file and loops through each line.
-    Each row is a candidate's results and there can be multiple
-    candidates in the csv. Then it writes that candidate's csv lines to a temp file.
+    Each line is a candidate's results and there can be multiple candidates in the csv. 
+    Then it writes that candidate's csv lines to a temp_csv string.
   """
+  temp_csv = ''
+
   with open(arg_map.get('candidate_votes_path'), 'r') as f:
     csv_reader = reader(f)
 
     for results in csv_reader:
-      write_candidate_csv_lines(results, arg_map)
+      temp_csv += write_candidate_csv_lines(results, arg_map)
+  
+  return temp_csv
 
 
 def write_candidate_csv_lines(row, arg_map):
   """
-    Writes a candidate's csv lines to a temp file.
+    Writes a candidate's csv lines to a csv_string.
 
     The program pauses once the candidate's lines have compiled,
     so the user can verify the output. Then it writes those lines to a temp file.
@@ -89,11 +92,10 @@ def write_candidate_csv_lines(row, arg_map):
   office = arg_map.get('office')
   district = arg_map.get('district', '')
   party = arg_map.get('party')
-  temp_file_name = arg_map.get('temp_file_name')
+  csv_string = ''
 
   if len(jurisdiction_list) == len(row):
     lines = []
-    csv_string = ''
 
     print('\n') # for the CLI display...
 
@@ -105,35 +107,25 @@ def write_candidate_csv_lines(row, arg_map):
     # pause execution to check work
     key_input = input('\nVerify the results are correct and press enter to continue. Press ctrl + c to reject.\n')
 
-    # finally, write the rows to the csv file
-    if not os.path.exists('tmp'):
-      os.makedirs('tmp')
-      
-    with open(temp_file_name, 'a') as f:
-      f.write(csv_string)
+    return csv_string
 
   else:
     print('Invalid inputs. There is likely a mismatch in how many jurisdictions are input and how many vote tallies were input for a candidate.')
     sys.exit()
 
 
-def sort_and_output_final_csv(arg_map):
+def sort_and_output_final_csv(tmp_csv, arg_map):
   """
-    Processes the temp file csv for final output.
+    Processes the tmp_csv for final output.
   """
-  tmp_file = arg_map.get('temp_file_name')
   results_file = arg_map.get('results_file_name')
   csv_header = arg_map.get('csv_header')
 
-  df = pd.read_csv(tmp_file, header=None)
+  df = pd.read_csv(StringIO(tmp_csv), header=None)
   df.columns = csv_header
   df.drop_duplicates(subset=None, inplace=True)
   df.sort_values(by=['candidate', 'county'], inplace=True)
   df.to_csv(results_file, index=False)
-
-  # delete tmp file
-  os.remove(tmp_file)
-  os.rmdir('tmp')
 
 
 if __name__ == '__main__':
--- todo.md
+++
@@ -1,8 +0,0 @@
-make tmp file directory go away after execution
-
-update counties.csv to jurisdictions.csv
-update readme arguments
-
-sort the multiple files
-
-use memory instead of tmp file
